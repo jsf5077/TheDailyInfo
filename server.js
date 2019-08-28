6fc4f89087eaa1ea5b05bfa16813926af3f67b3e
jsf5077 <jsf5077@gmail.com>
2019-08-28 01:52:55 -0400

    setup for Heroku Deployment

--- server.js
+++ server.js
@@ -200,6 +200,13 @@ app.put("/delete/:id", function(req, res) {
     });
 });
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
